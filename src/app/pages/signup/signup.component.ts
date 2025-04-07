694de3eafaf71a7ac69f87750e55053d473eab81
Végh Noémi <h379600@stud.u-szeged.hu>
2025-04-07 19:48:23 +0200

    bejelentkezés/regisztráció

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { NgForOf, NgIf } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, NgForOf, NgIf, RouterModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
